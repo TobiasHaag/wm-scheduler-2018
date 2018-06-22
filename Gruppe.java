fae82bdaf137bee68abdb22017b60ee111a36d03
Tobias Haag <tobias.haag@hfg-gmuend.de>
2018-06-23 00:00:21 +0200

    Kommentare

--- Gruppe.java
+++ Gruppe.java
@@ -12,7 +12,7 @@ public class Gruppe
 {
     private String gruppenname;
     private int anzahlNationen;
-    private HashMap<String, Nation>nationen;
+    private HashMap<String, Nation>nationen; // HashMap mit String als key und Nationen-Objekten als Value
     private HashMap<String, String>spielphasen;
     private ArrayList <String> elemente;
     private ArrayList <String> alleNationen;
@@ -21,17 +21,17 @@ public class Gruppe
      */
     public Gruppe(String name)
     {
-        erstelleNationenMap(); //Erzeugung der Hashmap mit den vorhandenen Nationen-Objekten.
         this.gruppenname = name; // Der Wert des Übergabeparameters name wird dem Attribut gruppenname zu gewiesen.
-        anzahlNationen = 4; // Setzt die Anzahl der Nationen in einer Gruppe statisch auf vier. Könnte auch dynamisch durch eine Variable definiert werden, anhand der Anzahl der Gruppen.
-        nationen = new HashMap<>();
+        nationen = new HashMap<>(); // Erzeugen der HashMap mit Nationen-Objekten.
+        erstelleNationenMap(); // Methodenaufruf, um die darin befindlichen Nationeninformationen der HashMap nationen zu übergeben.
+        anzahlNationen = nationen.size(); // Setzt die Anzahl der Nationen in einer Gruppe auf die Anzahl der befindlichen Objekte in der Hashmap.
         spielphasen = new HashMap<>();
         elemente = new ArrayList<>();
         alleNationen = new ArrayList<>();
     }
 
     /**
-     * 
+     * Die Methode 
      */
     private void erstelleNationenMap()
     {
@@ -41,6 +41,7 @@ public class Gruppe
             nationen.put("Saudi-Arabien", new Nation("Saudi-Arabien", 0, 0));
             nationen.put("Ägypten", new Nation("Ägypten", 0, 0));
             nationen.put("Uruguay", new Nation("Uruguay", 0, 0));
+            System.out.println("Die Nationen der Gruppe " + gruppenname + " wurde hinzugefügt!");
         }
         //Gruppe B
         if (gruppenname == "B"){
@@ -48,6 +49,7 @@ public class Gruppe
             nationen.put("Iran", new Nation("Iran", 0, 0));
             nationen.put("Portugal", new Nation("Portugal", 0, 0));
             nationen.put("Spanien", new Nation("Spanien", 0, 0));
+            System.out.println("Die Nationen der Gruppe " + gruppenname + " wurde hinzugefügt!");
         }
         //Gruppe C
         if (gruppenname == "C"){
@@ -55,6 +57,7 @@ public class Gruppe
             nationen.put("Australien", new Nation("Australien", 0, 0));
             nationen.put("Peru", new Nation("Peru", 0, 0));
             nationen.put("Dänemark", new Nation("Dänemark", 0, 0));
+            System.out.println("Die Nationen der Gruppe " + gruppenname + " wurde hinzugefügt!");
         }
         //Gruppe D
         if (gruppenname == "D"){
@@ -62,6 +65,7 @@ public class Gruppe
             nationen.put("Island", new Nation("Island", 0, 0));
             nationen.put("Kroatien", new Nation("Kroatien", 0, 0));
             nationen.put("Nigeria", new Nation("Nigeria", 0, 0));
+            System.out.println("Die Nationen der Gruppe " + gruppenname + " wurde hinzugefügt!");
         }
         //Gruppe E
         if (gruppenname == "E"){
@@ -69,6 +73,7 @@ public class Gruppe
             nationen.put("Serbien", new Nation("Serbien", 0, 0));
             nationen.put("Brasilien", new Nation("Brasilien", 0, 0));
             nationen.put("Schweiz", new Nation("Schweiz", 0, 0));
+            System.out.println("Die Nationen der Gruppe " + gruppenname + " wurde hinzugefügt!");
         }
         //Gruppe F
         if (gruppenname == "F") {
@@ -76,6 +81,7 @@ public class Gruppe
             nationen.put("Mexiko", new Nation("Mexiko", 0, 0));
             nationen.put("Schweden", new Nation("Schweden", 0, 0));
             nationen.put("Südkorea", new Nation("Südkorea", 0, 0));
+            System.out.println("Die Nationen der Gruppe " + gruppenname + " wurde hinzugefügt!");
         }
         //Gruppe G
         if (gruppenname == "G") {
@@ -83,6 +89,7 @@ public class Gruppe
             nationen.put("Panama", new Nation("Panama", 0, 0));
             nationen.put("Tunesien", new Nation("Tunesien", 0, 0));
             nationen.put("England", new Nation("England", 0, 0));
+            System.out.println("Die Nationen der Gruppe " + gruppenname + " wurde hinzugefügt!");
         }
         //Gruppe H
         if (gruppenname == "H") {
@@ -90,6 +97,12 @@ public class Gruppe
             nationen.put("Senegal", new Nation("Senegal", 0, 0));
             nationen.put("Kolumbien", new Nation("Kolumbien", 0, 0));
             nationen.put("Japan", new Nation("Japan", 0, 0));
+            System.out.println("Die Nationen der Gruppe " + gruppenname + " wurde hinzugefügt!");
+        }
+        
+        if (gruppenname != "A"){
+            System.out.println("Die erstellte Gruppe" + "" + "beinhaltet keine Nationen.");
+            System.out.println("Um eine Gruppe mit Nationen zu laden, gebe eines dieser Gruppen: A, B, C, D, E, F, G, H ein.");
         }
     }
 
@@ -108,36 +121,41 @@ public class Gruppe
      */
     public int anzahlNationen()
     {
-        anzahlNationen = nationen.size(); // size - Wird verwendet, um die Anzhal der Schlüssel/Wert-Zuordnung in der Map zurückzugeben.
+        anzahlNationen = nationen.size(); // size - Wird verwendet, um die Anzahl der Schlüssel/Wert-Zuordnung in der Map zurückzugeben.
         return anzahlNationen;
     }
     
     /**
-     * Set-Methode, die .
-     * @return die Anzahl der Nationen als Integer-Wert.
+     * Erstelle ein neues Objekt vom Typ Nation, speichere es in die HashMap nationen und aktualisiere die Nationenanzahl.
      */
-    public void erstelleNation(String name)
+    public void erstelleNeueNation(String name)
     {
-        Nation nation = new Nation(name, 0, 0);
-        nationen.put(name, nation);
+        Nation nation = new Nation(name, 0, 0); // new - Über den Konstruktor der Klasse Nation wird ein neues Nationen-Objekt erzeugt.
+        nationen.put(name, nation); // put - Fügt in die HashMap ein neues Objekt hinzu.
         anzahlNationen();
     }
 
+    /**
+     * In die HashMap wird ein neues Objekt Nation mit Name, Tore und Punkte hinzugefügt.
+     */
     public void fügeNationInGruppe(String name, int tore, int punkte)
     {
-        nationen.put(name, new Nation(name, tore, punkte));
+        nationen.put(name, new Nation(name, tore, punkte)); //  Über den Konstruktor der Klasse Nation wird ein neues Nationen-Objekt erzeugt.
+        anzahlNationen();
     }
     
     /**
-     *
+     * Methode mit einer Boolean-Abfrage, die Nationen in der HashMap nach Namen überprüft.
+     * Dabei wird der Key (Name der Nation als String) mit der Eingabe verglichen.
      */
     public boolean prüfeNation(String name)
     {
+        // containsKey - Überprüft, die Zuordnung für den angegebenen Schlüssel.
         if(nationen.containsKey(name)){
-            return true;
+            return true; // Namen stimmen überein - Nation befindet sich bereits in der Hashmap.
         }
         else{
-            return false;
+            return false; // Namen stimmen nicht überein - Nation befindet sich noch nicht in der Hashmap.
         }
     }
     
@@ -207,10 +225,11 @@ public class Gruppe
     }
     
     /**
-     * 
+     * Entfernt alle Nationen in der HashMap.
      */
     public void entferneNationen()
     {
-        nationen.clear();
+        nationen.clear(); // clear - Wird verwendet, um alle Zuordnungen der Nationen-Objekte zu entfernen.
+        anzahlNationen();
     }
 }
